a38af1c31ae31b35048c5c49e83f5d6a20788879
Sergei Wallace <spwallace1016@gmail.com>
2016-06-20 14:16:50 -0400

    fixed Force-Directed Graph with Drag/Zoom/Pan/Center/Resize/Labels/Shapes/Filter/Highlight

--- graph/graph_visualization.py
+++ graph/graph_visualization.py
@@ -10,19 +10,37 @@ import flask
 
 
 def main():
-    graph = import_graph("./data/nodes.csv", "./data/edges.csv")
+    graph = import_graph("nodes.csv", "edges.csv")
     #plot_graph(graph)
-    graph_browser(graph)
+    # graph_browser(graph)
+    # graph_browser()
+
+
+def graph_browser(graph=None):
+    if graph != None:
+        graph_json = json_graph.node_link_data(graph)
+        with open('graph.json', 'w') as g:
+            json.dump(graph_json, g)
+
+    # Serve the file over http to allow for cross origin requests
+    app = flask.Flask(__name__, static_folder="graph")
+
+    @app.route('/<path:path>')
+    def static_proxy(path):
+        return app.send_static_file(path)
+
+    port_num = 7999
+    print('\nGo to http://localhost:' + str(port_num) + '/graph/index.html to see the example\n')
+    app.run(port=port_num)
+    http_server.load_url('./graph/index.html')
 
 
 def import_graph(node_file, edge_file):
     graph = nx.Graph()
     nodeDF = pd.read_csv(node_file)
     nodeDF = nodeDF.values.tolist()
-    #print(nodeDF.dtypes)
     edgeDF = pd.read_csv(edge_file)
     edgeDF = edgeDF.values.tolist()
-    print(edgeDF[0][2])
     for i in range(len(nodeDF)):
         graph.add_node(nodeDF[i][0], name=nodeDF[i][1], phone=nodeDF[i][2], email= nodeDF[i][3], IP=nodeDF[i][4], clusterID=nodeDF[i][5])
     for i in range(len(edgeDF)):
@@ -34,25 +52,7 @@ def import_graph(node_file, edge_file):
 def plot_graph(graph):
     # plot graph in matplotlib
     nx.draw(graph)
-    plt.savefig("graph_images/demo_graph.png")  # save as png
+    plt.savefig("../graph_images/demo_graph.png")  # save as png
     plt.show()  # display graph
 
-def graph_browser(graph):
-    graph_json = json_graph.node_link_data(graph)
-    with open('./graph/graph.json', 'w') as g:
-        json.dump(graph_json, g)
-
-    #http_server.load_url('./force/force.html')
-    #'''
-    # Serve the file over http to allow for cross origin requests
-    app = flask.Flask(__name__, static_folder="graph")
-
-    @app.route('/<path:path>')
-    def static_proxy(path):
-        return app.send_static_file(path)
-
-    print('\nGo to http://localhost:8080/graph/graph.html to see the example\n')
-    app.run(port=8080)
-    http_server.load_url('./graph/graph.html')
-    #'''
 main()
